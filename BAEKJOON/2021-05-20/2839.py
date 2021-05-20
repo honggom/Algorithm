6f7d2d7707264cbbc08ca138aa0f4fa6c78914d7
honggom <cavok699@naver.com>
2021-05-20 16:33:12 +0900

    2021-05-20

---
+++ BAEKJOON/2021-05-20/2839.py
@@ -0,0 +1,16 @@
+n = int(input())
+c3 = 0
+
+if n % 5 == 0:
+    print(n // 5)
+else:
+    while True:
+        if n - 5 < 0 and n - 3 < 0:
+            print(-1)
+            break
+        else:
+            n -= 3
+            c3 += 1
+            if n % 5 == 0:
+                print(c3 + (n // 5))
+                break
