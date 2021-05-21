4c52b79201524f828ea024370b4387485b00dfa5
honggom <cavok699@naver.com>
2021-05-21 16:12:57 +0900

    2021-05-21

--- BAEKJOON/2021-05-21/1978.py
+++ BAEKJOON/2021-05-21/1978.py
@@ -1,3 +1,4 @@
+# 소수 구하기 1
 int(input())
 n = list(map(int, input().split()))
 count = 0
@@ -9,6 +10,6 @@ for i in n:
                 arr.append(i / (j+1))
         if len(arr) == 2:
             count += 1
-            print("%d는 소수임!" % (i))
+            #print("%d는 소수임!" % (i))
         arr = []
 print(count)
