735f69c6b92bf9bec3591483b539d75c370b9ba3
honggom <cavok699@naver.com>
2021-05-24 17:31:55 +0900

    2021-05-24

--- BAEKJOON/2021-05-23/1929.py
+++
@@ -1,9 +0,0 @@
-# 에라토스테네스의 체
-import sys
-m, n = map(int, sys.stdin.readline().split())
-
-for i in range(m, n+1):
-    if i == 2 or i == 3:
-        print(i)
-    elif i % 2 != 0 and i % 3 != 0:
-        print(i)
